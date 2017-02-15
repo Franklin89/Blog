250a46e843896a5e810c82018ffefcdb78f0cdd9
Matteo <matteolocher@gmail.com>
2017-02-15 16:48:33 +0100

    No idea why site.min.css is not be published. trying to rename it

--- src/MLSoftware.Web/gulpfile.js
+++ src/MLSoftware.Web/gulpfile.js
@@ -42,8 +42,8 @@ paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.concatJsDest = paths.webroot + "js/mlsoftware.min.js";
+paths.concatCssDest = paths.webroot + "css/mlsoftware.min.css";
 
 gulp.task("lib", ["clean"], function () {
     return gulp.src(library.source, { base: library.base })
