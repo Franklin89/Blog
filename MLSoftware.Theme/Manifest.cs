d3213449580ad5a3aabb9410b7093ab7a87e99b5
Matteo <matteolocher@gmail.com>
2018-08-20 13:56:17 +0200

    Feature/orchard core (#31)
    * Initial project structure
    
    * Update to latest orchard core and clean up solution
    
    * Add custom theme
    
    * Add syntax coloring
    
    * add app_data to gitignore
    
    * remove old launch and tasks json
    
    * add code snippet for cookie policy
    
    * Add Disqus blog comments
    
    * update layout

---
+++ MLSoftware.Theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "MLSoftware",
+    Author = "MLSoftware",
+    Website = "https://ml-software.ch",
+    Version = "1.0.0",
+    Description = "MLSoftware.Theme"
+)]
